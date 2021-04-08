88c1d854f962ca7e5d25fde867bef188fcabf57e
Jolud95 <www.appel@mail.ru>
2021-04-08 22:58:55 +0300

    creating dispatch

--- src/App.js
+++ src/App.js
@@ -15,13 +15,12 @@ const App = (props) => {
                 <Navbar/>
                 <div className="app-wrapper-content">
                     <Route path="/dialogs"
-                           render={ () =>
-                               <Dialogs state={props.state.dialogsPage}/> } />
+                           render={() =>
+                               <Dialogs state={props.state.dialogsPage}/>}/>
                     <Route path="/profile"
-                           render={ () =>
+                           render={() =>
                                <Profile profilePage={props.state.profilePage}
-                                        addPost={props.addPost}
-                                        updateNewPostText={props.updateNewPostText}/> } />
+                                        dispatch={props.dispatch}/>}/>
                 </div>
             </div>
         </BrowserRouter>);
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -6,11 +6,12 @@ const MyPosts = (props) => {
     let postElements = props.posts.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
     let newPostElement = React.createRef();
     let addPost = () => {
-        props.addPost();
+        props.dispatch({type: "ADD-POST"});
     }
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        let action = {type: "UPDATE-NEW-POST-TEXT", newText: text};
+        props.dispatch(action);
     }
     return (
         <div className={s.postsBlock}>
