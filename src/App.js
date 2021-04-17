11695ce9db37403b6fc767ee5f1d246f19067278
Jolud95 <www.appel@mail.ru>
2021-04-17 13:15:27 +0300

    Context

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ const App = (props) => {
                 <div className="app-wrapper-content">
                     <Route path="/dialogs"
                            render={() =>
-                               <DialogsContainer store={props.store}/>}/>
+                               <DialogsContainer />}/>
                     <Route path="/profile"
                            render={() =>
-                               <Profile store={props.store}/>}/>
+                               <Profile />}/>
                 </div>
             </div>
         </BrowserRouter>);
--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import store from "./redux/redux-store";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import StoreContext, {Provider} from "./StoreContext";
 
 
 
 export let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state}
-                 dispatch={store.dispatch.bind(store)}
-                 store={store}
-            />
+            <Provider store={store}>
+            <App />
+            </Provider>
         </React.StrictMode>,
         document.getElementById('root')
     );
