08b3626210d0a42a901077f7994c29fd20eb3eca
Jolud95 <www.appel@mail.ru>
2021-05-01 18:16:31 +0300

    Class component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,21 +1,18 @@
+import React from 'react';
 import styles from './Users.module.css'
 import avatar from "../../assets/images/avatar.png"
 import * as axios from "axios";
 
-    let Users = (props) => {
-
-
-    let getUsers = () => {
-        if (props.users.length === 0) {
-             axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    props.setUsers(response.data.items);
-                });
-        }
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {props.users.map(u => <div key={u.id}>
+    render() {
+        return <div>
+            {this.props.users.map(u => <div key={u.id}>
             <span>
                 <div>
                     <img src={u.photos.small != null ? u.photos.small : avatar} className={styles.userPhoto}/>
@@ -23,15 +20,15 @@ import * as axios from "axios";
                 <div>
                     {u.followed
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+                            this.props.unfollow(u.id)
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            props.follow(u.id)
+                            this.props.follow(u.id)
                         }}>Follow</button>
                     }
                 </div>
             </span>
-            <span>
+                <span>
                 <span>
                     <div>{u.name}</div>
                     <div>{u.status}</div>
@@ -41,9 +38,11 @@ import * as axios from "axios";
                     <div>{"u.location.city"}</div>
                 </span>
             </span>
-        </div>)
-        }
-    </div>
+            </div>)
+            }
+        </div>
+    }
+
 }
 
 
